e773515582a572081dd292fdfe11c5a7653ebbfa
Anh Vo <votqanh@gmail.com>
2023-11-16 01:58:01 -0800

    fixed url parsing and data collection

--- backend/django_app/views.py
+++ backend/django_app/views.py
@@ -17,11 +17,17 @@ def handle_frontend_data(request):
 
         print(field1, field2);
 
+        for input in [field1, field2]:
+            if not is_youtube_url(input):
+                print("Not a valid YouTube URL.")
+                # TODO: ask for input again
+
         youtube_service = get_authenticated_service('youtube', 'v3', api_key=settings.YOUTUBE_API_KEY)
         playlist1_items = get_playlist_items(youtube_service, field1)
         playlist2_items = get_playlist_items(youtube_service, field2)
 
-        process_playlist_items(playlist1_items)
+        playlist1_words = process_playlist_items(youtube_service, playlist1_items)
+        playlist2_words = process_playlist_items(youtube_service, playlist2_items)
 
         return JsonResponse({'message': 'Data received successfully'})
 
